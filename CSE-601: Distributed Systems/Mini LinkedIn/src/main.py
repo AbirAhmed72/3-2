741f120b84afa053a41ebb773666885784512810
AbirAhmed72 <abirahmed14872@gmail.com>
2023-07-21 19:35:02 +0600

    register api works

--- CSE-601: Distributed Systems/Mini LinkedIn/src/main.py
+++ CSE-601: Distributed Systems/Mini LinkedIn/src/main.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+import random
+import joblib as jb
+import models, schemas, services
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from typing import List, Optional
 
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.sql.expression import false
+from sqlalchemy.sql.sqltypes import Integer
+
+from schemas import TokenData
+from sqlalchemy.orm import Session
 from database import SessionLocal, engine
 
+from datetime import datetime, timedelta, time
+from jose import JWTError,jwt
 
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.add_middleware(
@@ -13,18 +27,45 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "token")
 
-# dependency(connecting db)
+# dependency
 def get_db():
     db = SessionLocal()
     try:
-        yield db 
+        yield db
     finally:
         db.close()
 
+credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not Validate the credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+        
+# def verify_user(token: str):
+#     try:
+#         payload = jwt.decode(token, services.SECRET_KEY, algorithms=[services.ALGORITHM])
+#         email: str = payload.get("sub")
+#         if email is None:
+#             raise credentials_exception
+#         token_data = schemas.TokenData(username = email)
+#         return token_data
+#     except JWTError:
+#         raise credentials_exception
+
 @app.post('/register')
-def register_user():
-    return {"message": "Hello World"}
+async def register_user(user_data: schemas.UserCreate,  db: Session = Depends(get_db)):
+    db_user = services.get_user_by_email(db, user_data.email)
+    if db_user:
+         raise HTTPException(status_code=400, detail="E-mail already Registered")
+    access_token_expires = timedelta(minutes=services.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = services.create_access_token(
+        data={"sub": user_data.email}, expires_delta=access_token_expires
+    )
+    data = services.create_user(db, user_data)
+    data.token = access_token
+    return data
 
 @app.post('/login')
 def login_user():
--- CSE-601: Distributed Systems/Mini LinkedIn/src/models.py
+++ CSE-601: Distributed Systems/Mini LinkedIn/src/models.py
@@ -7,30 +7,36 @@ from sqlalchemy import TIMESTAMP, Column, Integer, String, Boolean, DateTime
 class User(Base):
     __tablename__ = 'users'
     uid = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), nullable=False)
+    name = Column(String(50), nullable=False)
     email = Column(String(100), nullable=False)
+    password_hashed = Column(String)
+    posts = relationship('Post', back_populates='users')
+    notifications = relationship('Notification', back_populates='users')
+
 
 class Post(Base):
     __tablename__ = 'posts'
     pid = Column(Integer, primary_key=True, index=True)
-    uid = Column(Integer, ForeignKey('users.user_id'), nullable=False)
     post_text = Column(String, nullable=False)
     image_url = Column(String(255))
     created_at = Column(TIMESTAMP)
+    uid = Column(Integer, ForeignKey('users.uid'), nullable=False)
 
     # Relationship with User table
-    user = relationship('User', back_populates='posts')
+    users = relationship('User', back_populates='posts')
+    notifications = relationship('Notification', back_populates='posts')
+
 
 class Notification(Base):
     __tablename__ = 'notifications'
     nid = Column(Integer, primary_key=True, index=True)
-    uid = Column(Integer, ForeignKey('users.user_id'), nullable=False)
-    pid = Column(Integer, ForeignKey('posts.post_id'), nullable=False)
+    uid = Column(Integer, ForeignKey('users.uid'), nullable=False)
+    pid = Column(Integer, ForeignKey('posts.pid'), nullable=False)
     notification_text = Column(String(50), nullable=False)
     is_read = Column(Boolean, default=False)
     created_at = Column(TIMESTAMP)
 
     # Relationship with User table
-    user = relationship('User', back_populates='notifications')
+    users = relationship('User', back_populates='notifications')
     # Relationship with Post table
-    post = relationship('Post', back_populates='notifications')
+    posts = relationship('Post', back_populates='notifications')
