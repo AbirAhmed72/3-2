247414df6614f3d04859e7a51fcbaaa68b5a0294
AbirAhmed72 <abirahmed14872@gmail.com>
2023-09-21 15:23:27 +0600

    cleaned up code for microservice distribution

---
+++ CSE-601: Distributed Systems/Mini LinkedIn Distributed/backend/src/models.py
@@ -0,0 +1,42 @@
+# models.py
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey, PrimaryKeyConstraint
+from database import Base
+
+from sqlalchemy import TIMESTAMP, Column, Integer, String, Boolean, DateTime
+
+class User(Base):
+    __tablename__ = 'users'
+    uid = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), nullable=False)
+    password_hashed = Column(String)
+    posts = relationship('Post', back_populates='users')
+    notifications = relationship('Notification', back_populates='users')
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    pid = Column(Integer, primary_key=True, index=True)
+    post_text = Column(String, nullable=False)
+    image_url = Column(String(255))
+    created_at = Column(TIMESTAMP)
+    username = Column(String(100), ForeignKey('users.username'), nullable=False)
+
+    # Relationship with User table
+    users = relationship('User', back_populates='posts')
+    notifications = relationship('Notification', back_populates='posts')
+
+
+class Notification(Base):
+    __tablename__ = 'notifications'
+    nid = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), ForeignKey('users.username'), nullable=False)
+    pid = Column(Integer, ForeignKey('posts.pid'), nullable=False)
+    notification_text = Column(String(50), nullable=False)
+    is_read = Column(Boolean, default=False)
+    created_at = Column(TIMESTAMP)
+
+    # Relationship with User table
+    users = relationship('User', back_populates='notifications')
+    # Relationship with Post table
+    posts = relationship('Post', back_populates='notifications')
